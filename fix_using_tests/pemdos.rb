5785b0ca7cdbe9998182689d6d01c3fb6c718767
Babamurat Jummyyev <muratj.tm@gmail.com>
2020-07-24 20:11:13 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,9 +3,10 @@ require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
     10.times do
-      
+      string = "s" + string
+    end
     
-    10 * "s" + string
+    # 10 * "s" + string
   else
     string
   end
